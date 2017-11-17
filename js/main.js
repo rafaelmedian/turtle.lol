8bf845ba87784c8a82f69e24a93f24da84710b93
Rafael Medina <rafaelmedina@outlook.com>
2017-11-17 06:12:15 -0500

    Video changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 (function(){$(".logo").click(function(){$("h1").toggleClass("contrast")}),$(document).ready(function(){var o;o=$(".click")[0],$(".logo").mousedown(function(){o.play()}),$(".logo").mouseup(function(){o.play()})})}).call(this);
+
+
+'use strict';
+
+var getTargetHTML = function getTargetHTML(elem) {
+
+	var id = elem.getAttribute('data-show-id');
+	var target = document.querySelector('[data-id="' + id + '"]');
+
+	return target.outerHTML;
+};
+
+Array.prototype.forEach.call(document.querySelectorAll('[data-show-id]'), function (elem) {
+
+	var html = getTargetHTML(elem);
+
+	elem.onclick = basicLightbox.create(html).show;
+});
